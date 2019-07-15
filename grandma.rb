5c2dada0f0de8587ecae94ec115956818cdf22bb
Veronica Lopez <nicaa0695@gmail.com>
2019-07-15 23:23:45 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -5,7 +5,7 @@ def speak_to_grandma(string)
     return "HUH?! SPEAK UP, SONNY!"
   elsif string.upcase
     return "NO, NOT SINCE 1938!"
-  elsif string == "I LOVE YOU GRANDMA!"
+  else string == "I LOVE YOU GRANDMA!"
   end
 end
 
